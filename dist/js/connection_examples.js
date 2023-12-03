93fb89dceecfb7672da2843e9c3b447114dde2d2
RicoKeijzer <ricokeijzer@gmail.com>
2023-12-03 23:01:32 +0200

    Added project section
    added styling and new logic for the project section.

---
+++ dist/js/connection_examples.js
@@ -0,0 +1,7 @@
+const games = [];
+games.add(
+  '{"game":1,"description":"day1","results":[{"description":"Places we technically lived in together","group":2,"items":["School", "Boat", "Tent", "Cottage"]},{"description":"Sports you learned last years","group":2,"items":["Bike", "Climb", "Ski", "Tennis"]},{"description":"things we did the weekend we met","group":3,"items":["Kayak", "Geocaching", "Cheers", "Swipe"]},{"description":"Few of our most life changing purchases","group":4,"items":["Apartment", "Car", "Coffee machine", "Rings"]}]}'
+);
+games.add(
+  '{"game":2,"description":"day2","results":[{"description":"Has/had questionable relation ships","group":1,"items":["Demian", "Ville", "Elena", "Colin"]},{"description":"Woman with wombs","group":2,"items":["Amy","Bonnie","Sofia","Mari"]},{"description":"People who will allways love you","group":3,"items":["Rico","Obi","Vladimir","Maria"]},{"description":"Sexy man that need their own xmas calander","group":4,"items":["Gleb","Giorgi","Stas","Khoa"]}]}'
+);
